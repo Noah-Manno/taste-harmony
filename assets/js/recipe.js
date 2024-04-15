975176b72adb82f07e600456bbe082149357153c
Noah Manno <noahmanno19@gmail.com>
2024-04-15 11:04:04 -0400

    added allergies and diets to pantry

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,19 +1,54 @@
-const ingredientsInput = $('#ingredientsInput')
-const addButton = $('#add')
+const ingredientsInput = $('#ingredientsInput');
+const allergiesInput = $('#allergies');
+const dietprefsInput = $('#dietprefs');
+const mealtypeInput = $('#mealtype');
+const addButton = $('#add');
+const addAllergy = $('#addAllergy');
+const addDiet = $('#addDiet');
+
+const includeIngredients = $('#includeIngredients');
+const includeAllergies = $('#includeAllergies');
+const includeDiets = $('#includeDiets');
 
 addButton.on('click', handleAddingIngredients)
+addAllergy.on('click', handleAddingIngredients)
+addDiet.on('click', handleAddingIngredients)
+
 
 function handleAddingIngredients() {
     // Retrieve Value From Input
     let newIngredient = ingredientsInput.val();
+    let newAllergy = allergiesInput.val();
+    let newDiet = dietprefsInput.val();
+    let newMeal = mealtypeInput.val();
     // Retrieve the current list of ingredients from local storage
     let ingredients = JSON.parse(localStorage.getItem('ingredients')) || [];
+    let allergies = JSON.parse(localStorage.getItem('allergies')) || [];
+    let diets = JSON.parse(localStorage.getItem('diets')) || [];
+    let meals = JSON.parse(localStorage.getItem('meals')) || [];
     // Add the new ingredient
     if (newIngredient) {
         ingredients.push(newIngredient);
         localStorage.setItem('ingredients', JSON.stringify(ingredients));
+        handleAddingIngredientsToPantry(ingredients);
+    }
+    if (newAllergy) {
+        allergies.push(newAllergy);
+        localStorage.setItem('allergies', JSON.stringify(allergies));
+        handleAddingAllergiesToPantry(allergies);
+    }
+    if (newDiet) {
+        diets.push(newDiet);
+        localStorage.setItem('diets', JSON.stringify(diets));
+        handleAddingDietsToPantry(diets);
+    }
+    if (newMeal) {
+        meals.push(newMeal);
+        localStorage.setItem('meals', JSON.stringify(meals));
     }
     handleAddingIngredientsToPantry(ingredients);
+    handleAddingAllergiesToPantry(allergies);
+    handleAddingDietsToPantry(diets);
 }
 
 $('#includeIngredients').on('click', '.delete-button', function () {
@@ -25,10 +60,31 @@ $('#includeIngredients').on('click', '.delete-button', function () {
     let ingredientToRemove = $(this).closest('li').find('span').text();
     ingredients = ingredients.filter(ingredient => ingredient !== ingredientToRemove);
     localStorage.setItem('ingredients', JSON.stringify(ingredients));
-})
+});
+
+$('#includeAllergies').on('click', '.delete-button', function () {
+    // Remove appended Item
+    $(this).closest('li').remove();
+
+    // Update Array and Local Storage
+    let allergies = JSON.parse(localStorage.getItem('allergies')) || [];
+    let allergyToRemove = $(this).closest('li').find('span').text();
+    allergies = allergies.filter(allergy => allergy !== allergyToRemove);
+    localStorage.setItem('allergies', JSON.stringify(allergies));
+});
+
+$('#includeDiets').on('click', '.delete-button', function () {
+    // Remove appended Item
+    $(this).closest('li').remove();
+
+    // Update Array and Local Storage
+    let diets = JSON.parse(localStorage.getItem('diets')) || [];
+    let dietToRemove = $(this).closest('li').find('span').text();
+    diets = diets.filter(diet => diet !== dietToRemove);
+    localStorage.setItem('diets', JSON.stringify(diets));
+});
 
 function handleAddingIngredientsToPantry(ingredients) {
-    const includeIngredients = $('#includeIngredients');
     includeIngredients.empty();
     ingredients.forEach(ingredient => {
         // Create New List Item
@@ -41,7 +97,41 @@ function handleAddingIngredientsToPantry(ingredients) {
         newItem.append(deleteButton);
         includeIngredients.append(newItem);
         // Clear Input
-        ingredientsInput.val('')
+        ingredientsInput.val('');
+    });
+}
+
+function handleAddingAllergiesToPantry(allergies) {
+    includeAllergies.empty();
+    allergies.forEach(allergy => {
+        // Create New List Item
+        let newItem = $('<li>');
+        // Create Delete Button
+        let deleteButton = $('<button>').addClass('delete-button').text('X');
+        // Append Elements
+        let newText = $('<span>').text(allergy)
+        newItem.append(newText);
+        newItem.append(deleteButton);
+        includeAllergies.append(newItem);
+        // Clear Input
+        allergiesInput.val('');
+    });
+}
+
+function handleAddingDietsToPantry(diets) {
+    includeDiets.empty();
+    diets.forEach(diet => {
+        // Create New List Item
+        let newItem = $('<li>');
+        // Create Delete Button
+        let deleteButton = $('<button>').addClass('delete-button').text('X');
+        // Append Elements
+        let newText = $('<span>').text(diet)
+        newItem.append(newText);
+        newItem.append(deleteButton);
+        includeDiets.append(newItem);
+        // Clear Input
+        dietprefsInput.val('');
     });
 }
 
